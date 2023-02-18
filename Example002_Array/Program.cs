3d52b7f1b286068e54e4b7aa290a97b3c02296b9
Irina Ignatenok <specterhauz@gmail.com>
2023-02-18 15:22:20 +0200

    lecture2

---
+++ Example002_Array/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// Metod 1
+// int max = a1;
+// if(b1 > max) max = b1;
+// if(c1 > max) max = c1;
+
+// if(a2 > max) max = a2;
+// if(b2 > max) max = b2;
+// if(c2 > max) max = c2;
+
+// if(a3 > max) max = a3;
+// if(b3 > max) max = b3;
+// if(c3 > max) max = c3;
+// Console.WriteLine(max);
+
+
+// Method 2
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}   
+
+int[] array = {15,26,38,49,526,65,74,83,192};
+array[0] = 12;
+// Console.WriteLine(array[0]);
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+// Console.WriteLine(max);
+
+// Method3
+
+// int max = Max(
+//     Max(a1, b1, c1), 
+//     Max(a2, b2, c2),
+//      Max(max1, max2, max3));
+// Console.WriteLine(max);
---
+++ Example003_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {10, 29,83,44,90,64,7,90,};
+
+int n = array.Length;
+int find = 90;
+int index =0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
